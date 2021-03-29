fd8903e8976d4a31d155d27629c3176fae0d359f
Karen Sniquer <kbsniquer@gmail.com>
2021-03-29 15:29:50 -0400

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <img :src="nasaObject.url" />
+    <h1>{{ nasaObject.title }}</h1>
+    <p>{{ nasaObject.explanation }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      nasaObject: {},
+    };
+  },
+  async created() {
+    // GET request using fetch with async/await
+    const res = await fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=NfcyTbFPzpRXHo1mBtpooTJ5WQ158AOK6aYzmwzq"
+    );
+    const data = await res.json();
+
+    console.log(data);
+    this.nasaObject = data;
+  },
+};
 </script>
 
 <style>
+img {
+  width: 80%;
+}
+
+p {
+  margin: 0 auto;
+  width: 75%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.container {
+  max-width: 80%;
+  margin: 0 auto;
+  overflow: auto;
+  border-radius: 5px;
 }
 </style>
